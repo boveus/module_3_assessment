d3914250d622c7c21eb5b1c9a1e34b39d5f52480
Brandon Stewart <boveus@gmail.com>
2017-11-01 12:07:15 -0600

    add pagination that makes an infinite loop. cool cool cool

--- app/services/bestbuy_service.rb
+++ app/services/bestbuy_service.rb
@@ -1,5 +1,22 @@
 class BestbuyService
-  def find_stores_by_zip(zip, page_size=10, radius=25)
-    HTTParty.get("https://api.bestbuy.com/v1/stores(area(#{zip.to_i},#{radius.to_i}))?format=json&show=city,longName,distance,phone,storeType&pageSize=#{page_size}&apiKey=#{ENV['bb_key']}")
+  def initialize
+    @total = 0
+    @pages = {}
   end
+  def find_stores_by_zip(zip, page=1, page_size=10, radius=25)
+    response = HTTParty.get("https://api.bestbuy.com/v1/stores(area(#{zip.to_i},#{radius.to_i}))?format=json&show=city,longName,distance,phone,storeType&pageSize=#{page_size}&apiKey=#{ENV['bb_key']}")
+    # paginate_request(response)
+    # binding.pry
+    response
+  end
+
+  # def paginate_request(response)
+  #   stores = response["stores"]
+  #   num = 0
+  #   while stores.count > stores.count % 10
+  #     num += 1
+  #     @pages["page_#{num}"] = stores.reverse.pop(10)
+  #   end
+  #   @pages
+  # end
 end
